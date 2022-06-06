28673e8d17c43660bcac4baf7895837164d237ef
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-06-06 19:51:56 +0200

    feat: add cookies chore: add github actions

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+/* global browser */
 
 browser.browserAction.setBadgeBackgroundColor({color: 'white'});
 browser.browserAction.setBadgeText({text: "off" });
@@ -6,7 +7,7 @@ let online=false;
 
 async function handleCreated(info) {
     if(online){
-        console.log('handleCreated:', info.id, info.url);
+        //console.log('handleCreated:', info.id, info.url);
 
         try {
             browser.downloads.cancel(info.id);
@@ -14,8 +15,44 @@ async function handleCreated(info) {
             console.log(`Error: ${e}`);
         }
 
+        // ref. https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/cookies/Cookie
+        // >>>
+        let cookie_query = {
+            firstPartyDomain: null // include both types of cookies
+        };
+        /*
+        if(info.url){
+            let tmp = new URL(info.url);
+            if( tmp.protocol === 'http:' || tmp.protocol === 'https:' ){
+                if(tmp.hostname){
+                    cookie_query["domain"] = tmp.hostname;
+                }
+            }
+
+        }
+        */
+        if(info.cookieStoreId) { cookie_query["storeId"] = info.cookieStoreId }
+        //console.log(JSON.stringify(cookie_query,null,4));
+
+        const cookies = (await browser.cookies.getAll(cookie_query)).map( (c) => {
+                    return {
+                         name: c.name
+                        ,value: c.value
+                        ,domain: c.domain
+                        ,hostOnly: c.hostOnly
+                        ,httpOnly: c.httpOnly
+                        ,path: c.path
+                        ,sameSite: c.sameSite
+                    };
+                });
+
+        // <<< cookies
+
+        const data = { info, cookies };
+        //console.log(JSON.stringify(data,null,4));
+
         try {
-            const resp =  await browser.runtime.sendNativeMessage("fwdl", info);
+            const resp =  await browser.runtime.sendNativeMessage("fwdl", data);
             console.log("Received " + resp);
         }catch(e) {
             console.log(`Error: ${e}`);
@@ -25,7 +62,6 @@ async function handleCreated(info) {
 }
 
 async function handleClick(tab) {
-
   // toggle state
   online = !online
   browser.browserAction.setBadgeText({tabId: tab.id, text: (online?"on":"off") });
@@ -33,5 +69,4 @@ async function handleClick(tab) {
 
 browser.downloads.onCreated.addListener(handleCreated);
 browser.browserAction.onClicked.addListener(handleClick);
-
 
